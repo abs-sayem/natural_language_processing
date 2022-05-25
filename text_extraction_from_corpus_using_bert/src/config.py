fb78760792b2dff04d520bf01d41ca9ff1870f2b
ABS-Sayem <abssayem121194@gmail.com>
2022-05-25 14:14:02 +0600

    deleted some file from nlp_basic directory

---
+++ text_extraction_from_corpus_using_bert/src/config.py
@@ -0,0 +1,14 @@
+import tokenizers
+import os
+
+MAX_LEN = 128
+TRAIN_BATCH_SIZE = 32
+VALID_BATCH_SIZE = 16
+EPOCHS = 10
+BERT_PATH = "../input/bert_base_uncased/"
+MODEL_PATH = "model.bin"
+TRAINING_FILE = "../input/train.csv"
+TOKENIZER = tokenizers.BertWordPieceTokenizer(
+    os.path.join(BERT_PATH, "vocab_text"),
+    lowercase=True
+)
