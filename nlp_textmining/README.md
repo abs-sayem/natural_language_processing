83a609abdaa82ac43043b0c244863a1d2ae8a8c1
Abs_Sayem <abssayem121194@gmail.com>
2022-06-13 16:18:07 +0600

    added nlp_textmining project

---
+++ nlp_textmining/README.md
@@ -0,0 +1,2 @@
+# textmining
+text generation, text preproceing,LSTM
---
+++ nlp_textmining/test.py
@@ -0,0 +1,15 @@
+#Master Branch
+print("New practice of git")
+a = input("")
+a = int(a)
+if a < 21:
+    print("He is not eligible for marrige")
+elif 21 < a:
+    print("He is eligible for marrige")
+
+for b in range(10):
+    print(b)
+# BranchA update
+for c in range(10):
+    for j in c:
+        print ("a")
